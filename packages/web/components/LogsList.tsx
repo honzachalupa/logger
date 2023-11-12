452ce181a37f61ef60da8ed48c8a920ed9ae6357
Jan Chalupa <janchalupa@outlook.cz>
2023-11-12 16:47:52 +0100

    chore: WIP.

--- packages/web/components/LogsList.tsx
+++ packages/web/components/LogsList.tsx
@@ -2,9 +2,75 @@
 
 import { Button, VirtualizedList } from "@honzachalupa/design-system";
 import { Log, LogActions, useLogger } from "@honzachalupa/logger";
+import cx from "classnames";
 import moment from "moment";
 import { useEffect, useState } from "react";
 import config from "../config";
+
+const Item: React.FC<any> = ({
+    namespaceId,
+    level,
+    message,
+    stack,
+    data,
+    timestamp,
+}) => {
+    const headerItems = [
+        {
+            label: "Timestamp",
+            value: moment(timestamp).format("D.M.YYYY HH:mm:ss"),
+        },
+        {
+            label: "Level",
+            value: level,
+        },
+        {
+            label: "App ID",
+            value: namespaceId,
+        },
+        {
+            label: "Message",
+            value: message,
+        },
+    ].filter(({ value }) => value);
+
+    return (
+        <article
+            className={cx("p-8 border-b-2 border-b-slate-800", {
+                "border-l-4 border-l-orange-600": level === "warning",
+                "border-l-4 border-l-red-600": level === "error",
+            })}
+        >
+            <header className="flex pb-5">
+                {headerItems.map(({ label, value }) => (
+                    <div key={label} className="mr-10">
+                        <p className="text-sm text-gray-500">{label}</p>
+
+                        <p>{value}</p>
+                    </div>
+                ))}
+            </header>
+
+            {stack && (
+                <div className="pb-5">
+                    <p className="text-sm text-gray-500">Stack</p>
+
+                    <pre className="whitespace-pre-wrap">{stack}</pre>
+                </div>
+            )}
+
+            {data && (
+                <div className="pb-5">
+                    <p className="text-sm text-gray-500">Data</p>
+
+                    <pre className="whitespace-pre-wrap">
+                        {JSON.stringify(data, null, 3)}
+                    </pre>
+                </div>
+            )}
+        </article>
+    );
+};
 
 export const LogsList: React.FC = () => {
     const { log } = useLogger(config.namespaceId);
@@ -33,75 +99,11 @@ export const LogsList: React.FC = () => {
 
     return (
         <>
-            <Button label="Add" onClick={handleAddLog} />
+            <Button label="Add test log" onClick={handleAddLog} />
 
             <VirtualizedList
                 items={data}
-                renderer={({
-                    namespaceId,
-                    level,
-                    message,
-                    stack,
-                    data,
-                    timestamp,
-                }) => {
-                    const headerItems = [
-                        {
-                            label: "Timestamp",
-                            value: moment(timestamp).format(
-                                "D.M.YYYY HH:mm:ss"
-                            ),
-                        },
-                        {
-                            label: "Level",
-                            value: level.toUpperCase(),
-                        },
-                        {
-                            label: "App ID",
-                            value: namespaceId,
-                        },
-                        {
-                            label: "Message",
-                            value: message,
-                        },
-                    ].filter(({ value }) => value);
-
-                    return (
-                        <article className="p-8 border-b-slate-800 border-b-2">
-                            <header className="flex pb-5">
-                                {headerItems.map(({ label, value }) => (
-                                    <div key={label} className="mr-10">
-                                        <p className="text-sm text-gray-500">
-                                            {label}
-                                        </p>
-
-                                        <p>{value}</p>
-                                    </div>
-                                ))}
-                            </header>
-
-                            {data && (
-                                <div className="mr-10">
-                                    <p className="text-sm text-gray-500">
-                                        Data
-                                    </p>
-
-                                    <pre>{JSON.stringify(data, null, 3)}</pre>
-                                </div>
-                            )}
-
-                            {stack && (
-                                <div className="mr-10">
-                                    <p className="text-sm text-gray-500">
-                                        Stack
-                                    </p>
-
-                                    <pre>{stack}</pre>
-                                </div>
-                            )}
-                        </article>
-                    );
-                }}
+                renderer={(props) => <Item {...props} />}
             />
         </>
     );
